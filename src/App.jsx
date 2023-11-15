8027b14b733c1c8e423e52e454e50f5e538e30a8
Yostin Tejaxun <yostintejaxun20@gmail.com>
2023-11-15 07:11:51 -0600

    Add context for user authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+// Importación de bibliotecas externas
 import React from "react";
 
+// Importación de páginas
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
+
+// Importación de Context
+import { useAuth } from "./context/AuthContext";
 
 function App() {
   return <div>App</div>;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
+// Importación de bibliotecas externas
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Importación de componentes
 import App from "./App.jsx";
+
+// Importación de Provider
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
