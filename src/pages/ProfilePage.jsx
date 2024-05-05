3798bfdf62950f211c6cd125f9c5fa429815ba66
tejaby <yostintejaxun20@gmail.com>
2024-05-05 04:10:19 -0600

    Fix feature to list notes

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -10,44 +10,35 @@ import { useAuth } from "../context/AuthContext";
 
 function ProfilePage() {
   const { setUser, token, setToken } = useAuth();
-  const [note, setNote] = useState([]);
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchNotes = async () => {
       try {
         const response = await listNotesService(token.access);
-        setNote(response);
+        setNotes(response);
       } catch (err) {
-        if (err.status === 401 && err.data.code === "token_not_valid") {
-          try {
-            const response = await refreshService({ refresh: token.refresh });
-            setToken(response);
-            localStorage.setItem("authTokens", JSON.stringify(response));
-          } catch (err) {
-            setUser(null);
-            setToken(null);
-            localStorage.removeItem("user");
-            localStorage.removeItem("authTokens");
-            toast.error(err.data.detail, {
-              position: toast.POSITION.BOTTOM_CENTER,
-              autoClose: 3000,
-              pauseOnFocusLoss: false,
-              pauseOnHover: false,
-            });
-          }
-        }
+        setUser(null);
+        setToken(null);
+        localStorage.removeItem("user");
+        localStorage.removeItem("authTokens");
+        toast.error(err.data.detail, {
+          position: toast.POSITION.BOTTOM_CENTER,
+          autoClose: 3000,
+          pauseOnFocusLoss: false,
+          pauseOnHover: false,
+        });
       }
     };
-
     if (token && token.access) {
-      fetchData();
+      fetchNotes();
     }
-  }, [token, setToken]);
+  }, [token]);
 
   return (
     <div>
       <h2>notes</h2>
-      {note.map((note) => (
+      {notes.map((note) => (
         <ul key={note.id}>
           <li>{note.title}</li>
         </ul>
